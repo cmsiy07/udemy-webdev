275214151ad54cd5d16001efabdc6c5c36fd77aa
Cheryl Siy <cherylmsiy@gmail.com>
2023-02-04 19:38:19 +0900

    finish logic and info passing in EJS

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,21 +2,37 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const { request } = require("express");
 
 const app = express();
 
+let items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", function (req, res) {
 
-  var today = new Date();
+  let today = new Date();
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-       res.sendFile(__dirname + "/index.html");
-  }else{
-    res.send("Boo. It's a weekday :(");
-
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
+  let day = today.toLocaleDateString("en-US", options)
+ 
+  res.render("list", { kindOfDay: day, newListItems: items });
   
 });
+
+
+app.post("/", function(req,res){
+  let item = req.body.newItem;
+  items.push(item)
+  res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
